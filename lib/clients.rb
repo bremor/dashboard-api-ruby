4110c11d39aecf05be9a3585538d2f87c63decfd
bremor <34525505+bremor@users.noreply.github.com>
2025-01-09 16:02:34 +1100

    Adding per_page parameter support for list_clients_in_network

--- lib/clients.rb
+++ lib/clients.rb
@@ -16,8 +16,8 @@ module Clients
   # Return an array of connected clients in the target network
   # @param [String] network_id the network UUID for the target network
   # @return [Array] an array of hashes with each client in the network
-  def list_clients_in_network(network_id)
-    make_api_call(("/networks/#{network_id}/clients"), :get)
+  def list_clients_in_network(network_id, per_page = 10)
+    make_api_call(("/networks/#{network_id}/clients?perPage=#{per_page}"), :get)
   end
 
 end
